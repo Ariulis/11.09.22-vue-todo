ebbc33eb74ba17dc77aa8010bcadf6922fcf52b2
Ariulis <andrey59995@gmail.com>
2022-09-12 18:53:52 +0300

    rebuild project

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
 	transpileDependencies: true,
 	publicPath:
-		process.env.NODE_ENV === "production" ? "" : "/",
+		process.env.NODE_ENV === "production" ? "11.09.22-vue-todo/" : "/",
 });
